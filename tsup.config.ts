7fb14dcca9e144dd8221155161752b63a49586f6
Yuki <yuki.akai.rz.212@gmail.com>
2026-04-12 01:37:24 +0700

    release: use cjs build

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['./src/index.ts'],
-  format: ['esm'],
+  format: ['cjs'],
   platform: 'node',
   dts: true,
   bundle: true,
